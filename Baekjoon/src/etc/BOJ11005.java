fc31aab445bd7e50e93661f2e9695dd239e5488e
ekfhdwld <mickey0426@naver.com>
2021-07-07 12:09:31 +0900

    BOJ 11005 진법변환

---
+++ Baekjoon/src/etc/BOJ11005.java
@@ -0,0 +1,25 @@
+package etc;
+
+import java.util.Scanner;
+
+public class BOJ11005 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		long n = sc.nextLong();
+		int b = sc.nextInt();
+		
+		char[] arr = new char[b];
+		for(int i=10; i<b; i++) arr[i] = (char)('A' + i - 10); 
+		
+		StringBuilder sb = new StringBuilder();
+		while(n > 0) {
+			int r = (int) n % b;
+			if(r < 10) sb.append(r);
+			else sb.append(arr[r]);
+			n /= b;
+		}
+		
+		System.out.println(sb.reverse().toString());
+	}
+}
