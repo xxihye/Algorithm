d8d49e762f7999bba3fc4c72bdd8c14294e87c66
ekfhdwld <mickey0426@naver.com>
2021-07-18 15:58:18 +0900

    boj 11728 divide and conquer

---
+++ Baekjoon/src/divideandconquer/BOJ11728.java
@@ -0,0 +1,35 @@
+package divideandconquer;
+
+import java.io.*;
+import java.util.*;
+
+public class BOJ11728 {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken()), 
+			m = Integer.parseInt(st.nextToken());
+		
+		int[] arrA = new int[n];
+		int[] arrB = new int[m];
+		
+		st = new StringTokenizer(br.readLine());
+		for(int i=0; i<n; i++) arrA[i] = Integer.parseInt(st.nextToken());
+		
+		st = new StringTokenizer(br.readLine());
+		for(int i=0; i<m; i++) arrB[i] = Integer.parseInt(st.nextToken());
+		
+		
+		int a = 0, b = 0;
+		while(a < n || b < m) {
+			if(a < n && (b == m || arrA[a] <= arrB[b])) bw.write(arrA[a++] + " ");
+			else bw.write(arrB[b++] + " ");
+		}
+		
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+}
