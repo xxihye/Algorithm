d7970a6684035af8e449d8d3f646d310d39040c2
ekfhdwld <mickey0426@naver.com>
2021-08-02 17:21:16 +0900

    modify boj9461

--- Baekjoon/src/dp/BOJ9461.java
+++ Baekjoon/src/dp/BOJ9461.java
@@ -1,22 +1,17 @@
 package dp;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.io.*;
+import java.util.*;
 
 public class BOJ9461 {
 
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
 		int t = Integer.parseInt(br.readLine());
 		
-		for(int i=0; i<t; i++) {
-			dp(Integer.parseInt(br.readLine()));
-		}
+		for(int i=0; i<t; i++) dp(Integer.parseInt(br.readLine()));
 		
-
+		br.close();
 	}
 	
 	public static void dp(int n) {
@@ -26,10 +21,10 @@ public class BOJ9461 {
 		if(n > 4) {
 			lengths[1] = lengths[2] = lengths[3] = 1;
 			lengths[4] = 2;
+			
 			for(int i=5; i<=n; i++) 
 				lengths[i] = lengths[i-1] + lengths[i-5];
 			
-			System.out.println(Arrays.toString(lengths));
 			System.out.println(lengths[n]);
 		}else if(n == 4) System.out.println(2);
 		else System.out.println(1);
