2740c8775990f28b513a265e3be3dc6939ed5ace
jihye <mickey0426@naver.com>
2021-09-24 16:45:23 +0900

    fastcampus algorithm

---
+++ Baekjoon/src/etc/BOJ2920.java
@@ -0,0 +1,32 @@
+package etc;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class BOJ2920 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int[] arr = new int[8];
+		String answer = "mixed";
+		for(int i=0; i<arr.length; i++) {
+			arr[i] = sc.nextInt();
+			if(!(arr[0] == 1 || arr[0] == 8)) break;
+			
+			if(i > 0) {
+				if(arr[i] - arr[i-1] == 1) answer = "ascending";
+				else if(arr[i] - arr[i-1] == -1) answer = "descending";
+				else {
+					answer = "mixed";
+					break;
+				}
+			}
+		}
+		
+		System.out.println(answer);
+		
+		
+		
+		
+	}
+}
