b3b71c00deb1edfdbbb34393fd3c181faeb325b9
jihye <mickey0426@naver.com>
2021-10-01 23:04:58 +0900

    leetcode twosum

---
+++ Leetcode/src/bruteforce/Twosum.java
@@ -0,0 +1,18 @@
+package bruteforce;
+
+import java.util.HashMap;
+
+public class Twosum {
+	public int[] twoSum(int[] nums, int target) {
+		HashMap<Integer, Integer> map = new HashMap<>();
+
+		for (int i = 0; i < nums.length; i++) {
+			if (map.containsKey(target - nums[i]))
+				return new int[] { i, map.get(target - nums[i]) };
+
+			map.put(nums[i], i);
+		}
+
+		throw new IllegalArgumentException("no solution");
+	}
+}
