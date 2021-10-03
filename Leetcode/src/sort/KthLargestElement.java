1de3a44f325655d843256f196885e89be8d087b7
jihye <mickey0426@naver.com>
2021-10-03 14:21:32 +0900

    leetcode kth missing positive number

---
+++ Leetcode/src/sort/KthLargestElement.java
@@ -0,0 +1,11 @@
+package sort;
+
+import java.util.Arrays;
+
+public class KthLargestElement {
+	public int findKthLargest(int[] nums, int k) {
+		Arrays.sort(nums);
+
+		return nums[nums.length - k];
+	}
+}
