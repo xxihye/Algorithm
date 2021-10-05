9f83f1df70ad60f6547ae5111fecc515e178e0d9
jihye <mickey0426@naver.com>
2021-10-05 22:03:52 +0900

    boj2455

---
+++ Baekjoon/src/implement/BOJ2455.java
@@ -0,0 +1,17 @@
+package implement;
+
+import java.util.Scanner;
+
+public class BOJ2455 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int max = 0, now = 0;
+		for(int i=0; i<4; i++) {
+			now += -sc.nextInt() + sc.nextInt();
+			max = max < now ? now : max;
+		}
+		
+		System.out.println(max);
+	}
+}
