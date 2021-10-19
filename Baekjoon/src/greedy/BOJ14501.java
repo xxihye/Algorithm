cde163c39dcc3029f8c62aa809a14533e016c3c9
jihye <mickey0426@naver.com>
2021-10-19 22:12:56 +0900

    입력 배열 수정

--- Baekjoon/src/greedy/BOJ14501.java
+++ Baekjoon/src/greedy/BOJ14501.java
@@ -7,19 +7,19 @@ public class BOJ14501 {
 		Scanner sc = new Scanner(System.in);
 		
 		int n = sc.nextInt();
-		int[] t = new int[n+1], p = new int[n+1];
+		int[][] tp = new int[n+1][n+1];
 		for(int i=1; i<=n; i++) {
-			t[i] = sc.nextInt();
-			p[i] = sc.nextInt();
+			tp[i][0] = sc.nextInt();
+			tp[i][1] = sc.nextInt();
 		}
 
 		//dp[n] : n번째 날에 일을 할 때 벌 수 있는 최고 금액
 		int[] dp = new int[n+1];
 		int ans = dp[0];
 		for(int i=1; i<=n; i++) {
-			if(i + t[i] <= n+1) {
+			if(i + tp[i][0] <= n+1) {
 				for(int j=0; j<i; j++) 
-					if(j + t[j] <= i) dp[i] = Math.max(dp[i], dp[j] + p[i]);
+					if(j + tp[j][0] <= i) dp[i] = Math.max(dp[i], dp[j] + tp[i][1]);
 				
 				ans = Math.max(ans, dp[i]);
 			}
