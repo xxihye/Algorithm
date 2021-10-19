b5f3145b0cf7c7845b9a85368d80522540362b65
jihye <mickey0426@naver.com>
2021-10-19 22:04:45 +0900

    boj 14719

---
+++ Baekjoon/src/implement/BOJ14719.java
@@ -0,0 +1,62 @@
+package implement;
+
+import java.util.*;
+
+public class BOJ14719 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int h = sc.nextInt();
+		int w = sc.nextInt();
+		
+		int[] arr = new int[w];
+		for(int i=0; i<w; i++) arr[i] = sc.nextInt();
+
+		LinkedList<Integer> leftList = new LinkedList<>(),
+							rightList = new LinkedList<>();
+		
+		int max = 0;
+		for(int i=0; i<w; i++) {
+			if(max < arr[i] || (max == arr[i] && max != 0)) {
+				max = arr[i];
+				leftList.add(i);
+			}
+		}
+		
+		max = 0;
+		for(int i=w-1; i>=0; i--) {
+			if(max < arr[i]) {
+				max = arr[i];
+				rightList.add(i);
+			}
+		}
+		
+		int ans = 0, height, left , right;
+		if(leftList.size() > 1) {
+			left = leftList.poll();
+			height = arr[left];
+			
+			while(!leftList.isEmpty()) {
+				right = leftList.peek();
+				for(int i=left+1; i<right; i++) 
+					ans += height - arr[i];
+				left = leftList.poll();
+				height = arr[left];
+			}
+		}
+		
+		if(rightList.size() > 1) {
+			right = rightList.poll();
+			height = arr[right];
+			
+			while(!rightList.isEmpty()) {
+				left = rightList.peek();
+				for(int i=left+1; i<right; i++) 
+					ans += height - arr[i];
+				right = rightList.poll();
+				height = arr[right];
+			}
+		} 
+		
+		System.out.println(ans);
+	}
+}
