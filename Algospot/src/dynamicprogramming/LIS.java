04a502db3dac496caeb9e98366f3dff14ecd8622
jihye <mickey0426@naver.com>
2022-01-26 16:52:53 +0900

    algospot LIS - public -> private으로 접근지정자 수정 - cache 배열 초기화 수정

--- Algospot/src/dynamicprogramming/LIS.java
+++ Algospot/src/dynamicprogramming/LIS.java
@@ -3,6 +3,7 @@ package dynamicprogramming;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class LIS {
@@ -27,8 +28,7 @@ public class LIS {
 		br.close();
 	}
 	
-	
-	public void set() throws IOException {
+	private void set() throws IOException {
 		n = Integer.parseInt(br.readLine());
 		arr = new int[n];
 		cache = new int[n+1];
@@ -36,20 +36,18 @@ public class LIS {
 		st = new StringTokenizer(br.readLine(), " ");
 		for(int i=0; i<n; ++i) {
 			arr[i] = Integer.parseInt(st.nextToken());
-			cache[i] = -1;
 		}
 		
-		cache[n] = -1;
+		Arrays.fill(cache, -1);
 	}
 	
-	public int lis(int start) {
+	private int lis(int start) {
 		if(cache[start+1] != -1) return cache[start+1];
 		
 		cache[start+1] = 1;
 		for(int next = start+1; next < n; ++next) {
 			if(start == -1 || arr[start] < arr[next]) cache[start+1] = Math.max(cache[start+1], lis(next)+1);
 		}
-		
 		return cache[start+1];
 	}
 }
