8438a7ed01fa9278359f924f47ffdddfe992423d
jihye <mickey0426@naver.com>
2022-05-31 11:54:41 +0900

    2021 Dev-Matching 웹 백엔드 개발 : 행렬 테두리 회전하기

--- Programmers/src/devMatching/RotateArrayBorder.java
+++ Programmers/src/devMatching/RotateArrayBorder.java
@@ -2,9 +2,6 @@ package devMatching;
 
 import java.util.*;
 
-/**
- * 2021 Dev-Matching : 웹 백엔드 개발 행렬 테두리 회전하기
- */
 public class RotateArrayBorder {
 
 	static int[][] arr;
@@ -22,22 +19,16 @@ public class RotateArrayBorder {
 
 		int num = 1;
 		arr = new int[rows][columns];
-		for (int i = 0; i < rows; i++)
+		for(int i = 0; i < rows; i++)
 			for (int j = 0; j < columns; j++)
 				arr[i][j] = num++;
 
-		
-		for(int[] tmp : arr)
-			System.out.println(Arrays.toString(tmp));
-		System.out.println();
-		
 		int[] res = new int[queries.length];
 		for (int q = 0; q < queries.length; q++) {
 			min = 10001;
 			arr = rotateBorder(queries[q][0] - 1, queries[q][1] - 1, queries[q][2] - 1, queries[q][3] - 1);
 			res[q] = min;
 		}
-
 		return res;
 	}
 
@@ -48,24 +39,16 @@ public class RotateArrayBorder {
 		
 		for (int x = x1; x <= x2; x++) {
 			for (int y = y2; y >= y1; y--) {
-				if (x1 < x && x < x2 && y1 < y && y < y2)
-					continue;
+				if (x1 < x && x < x2 && y1 < y && y < y2) continue;
 
 				if (x == x1) temp[x][y] = (y == y1) ? arr[x + 1][y] : arr[x][y - 1];
-				else if (x == x2)
-					temp[x][y] = y == y2 ? arr[x - 1][y] : arr[x][y + 1];
-				else
-					temp[x][y] = y == y1 ? arr[x + 1][y] : arr[x - 1][y];
-
+				else if (x == x2) temp[x][y] = y == y2 ? arr[x - 1][y] : arr[x][y + 1];
+				else temp[x][y] = y == y1 ? arr[x + 1][y] : arr[x - 1][y];
+				
 				min = temp[x][y] < min ? temp[x][y] : min;
 			}
 		}
 
-		for (int[] i : temp)
-			System.out.println(Arrays.toString(i));
-
-		System.out.println();
 		return temp;
 	}
-
 }
